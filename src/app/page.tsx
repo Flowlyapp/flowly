db07e6f5e4d44c84bf42570dd92cdbdcd82433cc
iko <iko@aiweapps.com>
2024-11-09 09:59:42 +0300

    telegram ui kit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
 'use client'
 
-import { ContentWrapper} from '@/components/ContentWrapper/ContentWrapper'
+import { ContentWrapper } from '@/components/ContentWrapper/ContentWrapper'
+import { AppRoot, Placeholder } from '@telegram-apps/telegram-ui';
 
 export default function Home() {
   return (
     <ContentWrapper>
-      <div>Hello Mini App</div>
+      <AppRoot>
+        <Placeholder
+          header="Hello mini app"
+          description="some description">
+          <img
+            alt="Telegram sticker"
+            src="https://xelene.me/telegram.gif"
+            style={{ display: 'block', width: '144px', height: '144px' }}
+          />
+        </Placeholder>
+      </AppRoot>
     </ContentWrapper>
   )
 }
